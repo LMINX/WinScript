0dee91ed48ccec29557d592f56e6df504b556707
LMINX <30495444+LMINX@users.noreply.github.com>
2017-12-18 16:42:19 +0800

    add PS Test Script

---
+++ Change_rtladm_PW.ps1
@@ -0,0 +1,14 @@
+﻿#################################################################################
+# !! Input server hostnames in c:\temp\servers.txt before you run below script!!#
+###
+###
+$computers = Get-Content -path C:\temp\servers.txt
+$password = Read-Host -prompt "Enter new password for user" -assecurestring
+$decodedpassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+Foreach($computer in $computers)
+{
+ $computer
+ $user = [adsi]"WinNT://$computer/rtladm,user"
+ $user.SetPassword($decodedpassword)
+ $user.SetInfo()
+}
